76451fff5f709363cbe25712138c6fd893d239fa
zenit710 <zenit710@gmail.com>
2023-03-21 00:43:04 +0100

    claeaner main

--- cmd/sokker-org-auto-bidder/main.go
+++ cmd/sokker-org-auto-bidder/main.go
@@ -18,63 +18,76 @@ func main() {
 		wrongSubcommand()
 	}
 
-	// create repository
-	playerRepository = player.NewSqlitePlayerRepository("./bidder.db")
-	if err := playerRepository.Init(); err != nil {
-		log.Print(err.Error())
-		return
-	}
+	playerRepository = createPlayerRepository()
 	defer playerRepository.Close()
 
+	// choose subcommand to run
+	switch os.Args[1] {
+	case "bid":
+		handleBidCommand()
+	case "add":
+		handleAddCommand()
+	default:
+		wrongSubcommand()
+	}
+}
+
+
+func createPlayerRepository() player.PlayerRepository {
+	playerRepository := player.NewSqlitePlayerRepository("./bidder.db")
+	if err := playerRepository.Init(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return playerRepository
+}
+
+func handleAddCommand() {
 	// define 'add' subcommand flags set
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	playerId := addCmd.Int("playerId", 0, "Player ID")
 	maxPrice := addCmd.Int("maxPrice", 0, "Maxium price for player to bid")
 
-	// choose subcommand to run
-	switch os.Args[1] {
-	case "bid":
-		log.Print("make bid for listed players:")
+	// parse cmd flags
+	addCmd.Parse(os.Args[2:])
 
-		// get players to bid list
-		players, err := playerRepository.GetList()
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
+	// validate playerId
+	if *playerId <= 0 {
+		log.Fatal("playerId has to be greater than zero")
+	}
 
-		// print all players to bid
-		for _, player := range players {
-			log.Printf("%v", player)
-		}
-	case "add":
-		// parse cmd flags
-		addCmd.Parse(os.Args[2:])
+	// validate maxPrice
+	if *maxPrice <= 0 {
+		log.Fatal("maxPrice has to be greater than zero")
+	}
 
-		// validate playerId
-		if *playerId <= 0 {
-			log.Fatal("playerId has to be greater than zero")
-		}
+	// create player model
+	player := &model.Player{
+		Id: uint(*playerId),
+		MaxPrice: uint(*maxPrice),
+	}
 
-		// validate maxPrice
-		if *maxPrice <= 0 {
-			log.Fatal("maxPrice has to be greater than zero")
-		}
+	// save player into the DB
+	if err := playerRepository.Add(player); err != nil {
+		log.Fatal(err)
+	}
 
-		// create player model
-		player := &model.Player{
-			Id: uint(*playerId),
-			MaxPrice: uint(*maxPrice),
-		}
+	log.Printf("player added to bid list: %v", player)
+}
 
-		// save player into the DB
-		if err := playerRepository.Add(player); err != nil {
-			log.Fatal(err)
-		}
+func handleBidCommand() {
+	log.Print("make bid for listed players:")
 
-		log.Printf("player added to bid list: %v", player)
-	default:
-		wrongSubcommand()
+	// get players to bid list
+	players, err := playerRepository.GetList()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	// print all players to bid
+	for _, player := range players {
+		log.Printf("%v", player)
 	}
 }
 
